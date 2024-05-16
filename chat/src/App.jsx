497276b2091475e8ed3f4de3f00362bd1bb72e6c
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2024-05-16 23:52:37 +0700

    UI login

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,122 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-import axios from 'axios';
-import LoginPopUp from './Login.jsx';
-import './App.css'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import LoginPage from './Login';
+import MainPage from './MainPage';
 
-const socket = io('http://localhost:3000');
-
-function App() {
-    const [messages, setMessages] = useState([]);
-    const [inputValue, setInputValue] = useState('');
-    const [username, setUsername] = useState('');
-    const [groups, setGroups] = useState([]);
-    const [selectedGroup, setSelectedGroup] = useState(null);
-
-    // useEffect(async () => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const res = await axios.get('/api/authenticate');
-    //             const Username = res.data.username;
-    //             setUsername(Username);
-        
-    //             const groupsRes = await axios.get(`/api/groups/${Username}`);
-    //             setGroups(groupsRes.data);
-    //         } catch (error) {
-    //             console.error('Error fetching data:', error);
-    //         }
-    //     };
-        
-    //     fetchData();
-        
-    // }, []);
-
-    // useEffect(()=>{
-    //     if (selectedGroup){
-    //         socket.emit('join', selectedGroup);
-
-            
-            
-    //         loadMessages();
-
-    //         socket.on('chat message', handleMessage);
-
-    //         return () => {
-    //             socket.off('chat message', handleMessage);
-    //             socket.emit('leave', selectedGroup);
-    //         };
-    //     }
-    // },[selectedGroup])
-    // const handleGroupSelect = (group) => {
-    //     setSelectedGroup(group);
-    //     setMessages([]); 
-    // };
-
-    // const handleMessage = (msg) => {
-    //     setMessages((prevMessages) => [...prevMessages, msg]);
-    // };
-
-    // const loadMessages = async () => {
-    //     try {
-    //         const response = await axios.get(`/api/groups/${selectedGroup}/0`);
-    //         response.data.forEach((message) => {
-    //             setMessages((prevMessages) => [...prevMessages, message.content]);
-    //         })
-    //     } catch (error) {
-    //         console.error('Error loading messages:', error);
-    //     }
-    // };
-    
-    
-
-    // const handleMessageSubmit = (e) => {
-    //     e.preventDefault();
-    //     if (inputValue.trim() !== '') {
-    //         socket.emit('chat message', {"content":inputValue, "chat_id":selectedGroup, "username" : username});
-    //         setInputValue('');
-    //     }
-    // };
-
+export default function App() {
     return (
-        <>
-            {/* {!username && <LoginPopUp/>}
-            {username && (
-                <div>
-                    <h1>Welcome, {username}</h1>
-                    <div>
-                        <h2>Select a Group</h2>
-                        <ul>
-                            {groups.map((group, index) => (
-                                <li key={index} onClick={() => handleGroupSelect(group.id)}>
-                                    {group.chatname}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    {selectedGroup && (
-                        <div>
-                            <ul>
-                                {messages.map((message, index) => (
-                                    <li key={index}>{message}</li>
-                                ))}
-                            </ul>
-                            <form onSubmit={handleMessageSubmit}>
-                                <input
-                                    type="text"
-                                    value={inputValue}
-                                    onChange={(e) => setInputValue(e.target.value)}
-                                />
-                                <button type="submit">Send</button>
-                            </form>
-                        </div>
-                    )}
-                </div> 
-            )} */}
-            <main className="bg">
-                <LoginPopUp/>
-            </main>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
--- chat/src/Login.jsx
+++ chat/src/Login.jsx
@@ -3,58 +3,59 @@ import axios from 'axios';
 import './Login.css'
 import icon from './assets/icon.png'
 
-const LoginPopUp = () => {
+const LoginPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const handleSubmit = async (event) => {
-        // event.preventDefault();      
-        // try {
+        event.preventDefault();      
+        try {
             
-        //     const res = await axios.post('/api/login', {
-        //         username: username,
-        //         password: password
-        //     });
+            const res = await axios.post('/api/login', {
+                username: username,
+                password: password
+            });
             
-        //     if (res.status >= 200 && res.status < 300) {
-        //         window.location.reload(); 
-        //     } else {
-        //         throw new Error('Login failed: Invalid credentials');
-        //     }
-        // } catch (err) {
+            if (res.status >= 200 && res.status < 300) {
+                window.location.reload(); 
+            } else {
+                throw new Error('Login failed: Invalid credentials');
+            }
+        } catch (err) {
             
-        //     alert('Login failed: ' + err.message);
-        // }
+            alert('Login failed: ' + err.message);
+        }
     };
     
     return (
-        <div className='loginBox'>
-            <div className='welcome'>
-                <p className='hello'>Hello, welcome back!</p>
-                <p className='greet'>It's great to see you again!</p>
-            </div>
-            <form className='form' onSubmit={handleSubmit}>
-                <div className='input'>
-                    <div className='part'>USERNAME</div>
-                    <input className='formInput' type="text" spellcheck='false' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <main className = 'bg'>
+            <div className='loginBox'>
+                <div className='welcome'>
+                    <p className='hello'>Hello, welcome back!</p>
+                    <p className='greet'>It's great to see you again!</p>
                 </div>
-
-                <div className='input'>
-                    <div className='part'>PASSWORD</div>
-                    <input className='formInput' type="password" spellcheck='false' value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-
-                <button className='login' type="submit" onClick={handleSubmit}>Login</button>
-
-                <div className='input'>
-                    <div style={ {color: '#B5BAC1', fontSize:'small', paddingTop: '5px'}}>Need an account? 
-                        <a href='/register' style={{paddingLeft:'10px'}}>Register</a>
+                <form className='form' onSubmit={handleSubmit}>
+                    <div className='input'>
+                        <div className='part'>USERNAME</div>
+                        <input className='formInput' type="text" spellcheck='false' value={username} onChange={(e) => setUsername(e.target.value)} />
                     </div>
-                </div>
-            </form>
-            <img src= {icon} style={{scale:'0.5', transform:'translate(150%,-180%)'}}></img>
-            {/* <div className='avatar'/> */}
-        </div>
+
+                    <div className='input'>
+                        <div className='part'>PASSWORD</div>
+                        <input className='formInput' type="password" spellcheck='false' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+
+                    <button className='login' type="submit" onClick={handleSubmit}>Login</button>
+
+                    <div className='input'>
+                        <div style={ {color: '#B5BAC1', fontSize:'small', paddingTop: '5px'}}>Need an account? 
+                            <a href='/register' style={{paddingLeft:'10px'}}>Register</a>
+                        </div>
+                    </div>
+                </form>
+                <img src= {icon} style={{scale:'0.5', transform:'translate(150%,-180%)'}}></img>
+            </div>
+        </main>
     )
 }
 
-export default LoginPopUp;
+export default LoginPage;
